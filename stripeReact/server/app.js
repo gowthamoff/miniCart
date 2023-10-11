36e80cbf927d5960f6cafd18879e6a0fe4d15e6b
Gowtham <127670808+gowthamoff@users.noreply.github.com>
2023-10-11 18:46:35 +0530

    init

--- stripeReact/server/app.js
+++ stripeReact/server/app.js
@@ -33,16 +33,16 @@ app.post("/api/create-checkout-session",async(req,res)=>{
         cancel_url:"http://localhost:3000/cancel",
     });
 
-    // const refund = await stripe.refunds.create({
-    //     payment_intent: 'pi_3NzdiPSD0yEYQlK20VXVRGUR',
-    // }); 
+    const refund = await stripe.refunds.create({
+        payment_intent: 'pi_3O026KSD0yEYQlK21XaiItsU',
+    }); 
     
-    const partial_refund = await stripe.refunds.create({
-        payment_intent: 'pi_3NzZstSD0yEYQlK21F3QIOn8',
-        amount: 1000,
-      });
+    // const partial_refund = await stripe.refunds.create({
+    //     payment_intent: 'pi_3NzZstSD0yEYQlK21F3QIOn8',
+    //     amount: 1000,
+    //   });
 
-    console.log(partial_refund)
+    console.log(refund)
 
     res.json({id:session.id})
  
