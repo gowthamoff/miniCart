66b74e795524232525ca2facef974b4f9366f065
Gowtham <127670808+gowthamoff@users.noreply.github.com>
2023-10-11 17:09:15 +0530

    working

--- stripeReact/server/app.js
+++ stripeReact/server/app.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(cors());
 
 
-// checkout api
+// checkout api working
 app.post("/api/create-checkout-session",async(req,res)=>{
     const {products} = req.body;
 
